a08d32bd927071c7ca0bb4f1a43a47ffc9533102
ClearingPath <13512093@std.stei.itb.ac.id>
2015-10-08 12:13:18 +0700

    added more and more features

--- src/main/java/com/lang/pat/rabbitirc/ClientMain.java
+++ src/main/java/com/lang/pat/rabbitirc/ClientMain.java
@@ -1,7 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * TODO :
+ * - Implement main menu and UI
+ * - Implement Thread for consumer (just run consumer.consume())
+ * - Implement user checking
+ * - Implement message constructor & destructor (JSON Preferable)
  */
 package com.lang.pat.rabbitirc;
 
@@ -17,6 +19,13 @@ public class ClientMain {
     public static String USERNAME;
     public static String QUEUENAME;
     public static String EXCHANGE_NAME = "lang.pat.rabbitIRC";
+    private Consumer consumer;
+    private Producer producer;
+    
+    public ClientMain(){
+        consumer = new Consumer();
+        producer = new Producer();
+    }
     
     public int JoinChannel(String Channel){
         for (String item : ChannelList){
@@ -27,6 +36,7 @@ public class ClientMain {
         }
         
         ClientMain.ChannelList.add(Channel);
+        consumer.AddChannel(Channel);
 //        System.out.println("Successful join : " + Channel);
         return 0;
     }
@@ -35,6 +45,7 @@ public class ClientMain {
         for (String item : ChannelList){
             if (item.equals(Channel)){
                 ClientMain.ChannelList.remove(Channel);
+                consumer.RemoveChannel(Channel);
 //                System.out.println("Successful leaving : " + Channel);
                 return 0;
             }
@@ -54,6 +65,20 @@ public class ClientMain {
         }
     }
     
+    public int Send(String Message){
+        int ret = 0;
+        //construct message first!
+        producer.send(Message);
+        return ret;
+    }
+    
+    public int Send(String Message, String ChannelName){
+        int ret = 0;
+        //construct message first!
+        producer.send(Message, ChannelName);
+        return ret;
+    }
+    
     public void Exit(){
         System.exit(0);
     }
@@ -62,7 +87,8 @@ public class ClientMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO implements menu and application
+        
     }
     
 }
--- src/main/java/com/lang/pat/rabbitirc/Consumer.java
+++ src/main/java/com/lang/pat/rabbitirc/Consumer.java
@@ -5,9 +5,12 @@
  */
 package com.lang.pat.rabbitirc;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
@@ -47,4 +50,30 @@ public class Consumer {
         return ret;
     }
     
+    public int RemoveChannel(String ChannelName){
+        int ret = 0;
+        try {
+            channel.queueUnbind(ClientMain.QUEUENAME, ClientMain.EXCHANGE_NAME, ChannelName);
+        } catch (IOException ex) {
+            Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+            ret = 1;
+        }
+        return ret;
+    }
+    
+    public void consume(){
+        com.rabbitmq.client.Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                    AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + envelope.getRoutingKey() + "':'" + message + "'");
+            }
+        };
+        try {
+            channel.basicConsume(ClientMain.QUEUENAME, true, consumer);
+        } catch (IOException ex) {
+            Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
